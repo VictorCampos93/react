f44a1913c4f6a56e448ac35e5dd43b4dfe13717c
VictorCampos93 <v.hugo93@hotmail.com>
2019-11-11 02:04:00 -0300

    Primeiras modificaçõs no cadastro

--- react-victor/src/App.js
+++ react-victor/src/App.js
@@ -1,24 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(1),
+    width: 400,
+  },
+  textFieldEndereco: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(1),
+    width: 300,
+  },
+  textFieldNumero: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(1),
+    width: 100,
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div id="titulo-propriedade">
+        Adicione uma Propriedade
+      </div>
+      <hr></hr>
+      <div className="box-menor">
+
+        <div className="box-content">
+
+          <TextField
+            id="standard-required"
+            label="Nome"
+            defaultValue=""
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="standard-required"
+            label="País"
+            defaultValue=""
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="standard-required"
+            label="Estado"
+            defaultValue=""
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="standard-required"
+            label="Cidade"
+            defaultValue=""
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            id="standard-required"
+            label="CEP"
+            defaultValue=""
+            className={classes.textField}
+            margin="normal"
+          />
+
+          <TextField
+            id="standard-required"
+            label="Endereço"
+            defaultValue=""
+            className={classes.textFieldEndereco}
+            margin="normal"
+            width="50"
+          />
+
+          <TextField
+            id="standard-required"
+            label="Numero"
+            defaultValue=""
+            className={classes.textFieldNumero}
+            margin="normal"
+            width="50"
+          />
+
+        </div>
+      </div>
+
+      <Button className="botao-primario" variant="contained">
+        Salvar
+      </Button>
+
+      <Button  className="botao-secundario" color="default" size="small">
+        Cancelar
+      </Button>
     </div>
   );
 }
